05495c31094d477a8ce395c41b008d540638d456
alinxess <alinxess1234@gmail.com>
2022-05-11 19:33:58 +0530

    Add all my files

---
+++ src/filezipper/FileZipper.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package filezipper;
+
+/**
+ *
+ * @author Alin Xess
+ */
+
+
+public class FileZipper {
+
+    public static void main(String[] args) {
+       new ZipperForm().setVisible(true);
+    }
+    
+}
